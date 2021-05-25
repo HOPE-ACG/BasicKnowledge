cf7f6d1207a15f06ab0b8c78ce2156b453b89248
ACG <18351572198@163.com>
2021-05-26 00:10:17 +0800

    Try update project from laptop.

--- DataStructureAndAlgorithm/src/main/java/dfsandbfs/DFS.java
+++ DataStructureAndAlgorithm/src/main/java/dfsandbfs/DFS.java
@@ -7,6 +7,7 @@ public class DFS {
     public static void dfs_matrix(AdjoiningMatrixMap map) {
 
         List<Character> vertexes = map.getVertexes();
+        int vertexesCount = map.getVertexesCount();
     }
 
     public static void dfs_linkedlist() {
